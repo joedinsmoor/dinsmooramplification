52f4e63f4d7e5ff3edb89139c0928d9b73ad02a1
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-08-01 22:53:57 -0400

    added site navigation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <nav>
+          <ul>
+            <li><strong>Home</strong></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="clients.html">Our Clients</a></li>
+            <li><a href="products.html">Our Products</a></li>
+            <li><a href="services.html">Our Services</a></li>
+            <li><a href="contact.html">Contact Us</a></li>
+          </ul>
+      </nav>
         <p>
           dinsmooramplification.com is under construction. Check back later!
         </p>
