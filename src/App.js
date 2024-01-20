9f7d67adba06b2a9421c9e2453f199c3e4b5ab39
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2024-01-20 17:41:49 -0500

    Moving to new framework

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import './index.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+// All pages
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import DemoProduct from './pages/DemoProduct';
+
+import {useDocTitle} from './components/CustomHook';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
+  useEffect(() => {
+    const aos_init = () => {
+      AOS.init({
+        once: true,
+        duration: 1000,
+        easing: 'ease-out-cubic',
+      });
+    }
+
+    window.addEventListener('load', () => {
+      aos_init();
+    });
+  }, []);
+
+  useDocTitle("MLD | Molad e Konsult - Bespoke Web and Mobile Applications");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <nav>
-          <ul>
-            <li><strong>Home</strong></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="modifications.html">Modification Requests</a></li>
-            <li><a href="products.html">Products</a></li>
-            <li><a href="services.html">Services</a></li>
-            <li><a href="contact.html">Contact</a></li>
-          </ul>
-      </nav>
-        <p>
-          dinsmooramplification.com is under construction. Check back later!
-        </p>
-      </header>
-    </div>
+    <>
+      <Router>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/get-demo" element={<DemoProduct />} /> 
+          </Routes>
+        </ScrollToTop>
+      </Router>
+    </>
   );
 }
+
 
 export default App;
